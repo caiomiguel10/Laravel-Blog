2162ac954d27708115c7d3ae4e40399e17fccb45
caiomiguel10 <caiomiguelmf@gmail.com>
2023-07-14 15:36:43 -0300

    readme

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -38,10 +38,17 @@
                     {{ __('Esqueceu a senha ?') }}
                 </a>
             @endif
+          
 
             <x-primary-button class="ml-3">
                 {{ __('Log in') }}
             </x-primary-button>
+            
         </div>
+        @if (Route::has('password.request'))
+        <a class=" text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+            {{ __('Registrar') }}
+        </a>
+    @endif
     </form>
 </x-guest-layout>
